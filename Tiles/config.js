d10bf21b36957e2e5a59e8dc6ac46f488ab9fbe7
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-04-18 13:57:10 +0100

    add tiles

---
+++ Tiles/config.js
@@ -0,0 +1,9 @@
+config={
+    "data": {
+        "n": 15
+    },
+    "style": {
+        "bgCol": "#B71540",
+        "colPalette": ["#E01A4F", "#F15946", "#F9C22E", "#53B3CB", "#7DCFB6"]
+    }
+}
