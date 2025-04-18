8610b8a11ab23a57679a140020b0ff81035a7a10
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-04-18 21:14:47 +0100

    add rectangles

--- Rectangles/config.js
+++ Rectangles/config.js
@@ -1,12 +1,16 @@
 config={
     "data": {
-        "n": 1500,
-        "rMin": 2,
-        "rMax": 15,
+        "nMin": 50,
+        "nMax": 100,
+        "rHeightMin": 50,
+        "rHeightMax": 100,
+        "rWidthMin": 50,
+        "rWidthMax": 75,
         "fillOpacityMin": 0.2,
         "fillOpacityMax": 0.8
     },
     "style": {
+        "strokeWidth": 2,
         "bgCol": "black",
         "colPalette": ["#F75C03", "#D90368", "#04A777", "#820263", "#F4E409"]
     }
---
+++ Rectangles/plot.js
@@ -0,0 +1,51 @@
+function rectanglesPlot() {
+  
+  const width = globalConfig.settings.width;
+  const height = globalConfig.settings.width;
+
+  const n = getRandomInt(config.data.nMin, config.data.nMax)
+
+  // Generate data
+  const data = d3.range(n).map(() => ({
+    x: d3.randomUniform(globalConfig.style.padding, globalConfig.settings.width - config.data.rWidthMax)(),
+    y: d3.randomUniform(globalConfig.style.padding, globalConfig.settings.width - config.data.rHeightMax)(),
+    rHeight: d3.randomUniform(config.data.rHeightMin, config.data.rHeightMax)(),
+    rWidth: d3.randomUniform(config.data.rWidthMin, config.data.rWidthMax)(),
+    fillCol: d3.randomUniform(0, 1)(),
+    fillOpacity: d3.randomUniform(config.data.fillOpacityMin, config.data.fillOpacityMax)()
+  }));
+
+  let colour = d3.scaleOrdinal(config.style.colPalette);
+
+  // Plot
+  const chartContainer = d3.select("#rectangles")
+    .style('background-color', config.style.bgCol)
+    .style('padding', globalConfig.style.padding + 'px')
+    .style('width', width + globalConfig.style.padding*2 + 'px');
+
+  const svg = chartContainer
+    .append("svg")
+    .attr("viewBox", `0 0 ${width} ${height}`)
+    .attr("preserveAspectRatio", "xMidYMid meet")
+    .style("width", "100%")
+    .style("height", "auto")
+    .append("g")
+
+  svg.selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('x', d => d.x)
+    .attr('y', d => d.y)
+    .attr('width', d => d.rWidth)
+    .attr('height', d => d.rHeight)
+    .style('fill', d => colour(d.fillCol))
+    .style('fill-opacity', d => d.fillOpacity)
+    .style('stroke', d => colour(d.fillCol))
+    .style('stroke-width', config.style.strokeWidth);
+
+};
+
+window.onload = function() {
+  rectanglesPlot()
+}
