bb37fde76804c6361a9f304433d9ea7b99c553a5
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-04-18 18:33:54 +0100

    Add bubbles disco

---
+++ BubblesDisco/config.js
@@ -0,0 +1,13 @@
+config={
+    "data": {
+        "n": 1500,
+        "rMin": 2,
+        "rMax": 15,
+        "fillOpacityMin": 0.2,
+        "fillOpacityMax": 0.8
+    },
+    "style": {
+        "bgCol": "black",
+        "colPalette": ["#F75C03", "#D90368", "#04A777", "#820263", "#F4E409"]
+    }
+}
