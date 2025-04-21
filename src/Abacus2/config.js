d98e0716ca3ef7cdc590984d27e9ed7a73d5fd65
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-04-21 21:01:54 +0100

    add abacus2

---
+++ src/Abacus2/config.js
@@ -0,0 +1,14 @@
+config={
+    "data": {
+        "n": 20,
+        "nCol": 20,
+        "rMin": 1,
+        "rMax": 25
+    },
+    "style": {
+        "bgCol": "#361134",
+        "mainCol": "#B0228C",
+        "strokeWidth": 0.9,
+        "fillOpacity": 0.5
+    }
+}
