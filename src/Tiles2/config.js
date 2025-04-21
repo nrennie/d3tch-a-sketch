21a7665a70785c30d31e8d5e442ad92d99e432c8
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-04-21 21:06:00 +0100

    add tiles2

---
+++ src/Tiles2/config.js
@@ -0,0 +1,9 @@
+config={
+    "data": {
+        "n": 7
+    },
+    "style": {
+        "bgCol": "black",
+        "colPalette": ["#ff9062", "#fd6598", "#cb64c0", "#3294dd", "#75fb8a", "#d0eb60"]
+    }
+}
---
+++ src/Tiles2/plot.js
@@ -0,0 +1,3 @@
+window.onload = function () {
+  tilesPlot("#tiles2");
+};
